61469d6c5831a6282eeffce50c3a078eeea214bf
Daniel Ma <dma@paloaltonetworks.com>
2026-09-01 09:47:30 +0800

    fix output

--- producer/outputs.tf
+++ producer/outputs.tf
@@ -5,9 +5,5 @@ output "PRODUCER_PROJECT" {
 
 output "DEPLOYMENT_GROUP" {
   description = "The ID of the producer deployment group (either Intercept or Mirroring)."
-  value = try(
-    google_network_security_intercept_deployment_group.main[0].id,
-    google_network_security_mirroring_deployment_group.main[0].id,
-    null
-  )
+  value       = var.mirroring_mode ? google_network_security_mirroring_deployment_group.main[0].id : google_network_security_intercept_deployment_group.main[0].id
 }
